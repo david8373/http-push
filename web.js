cd9fd3eda004698f4fc9a8622efc6645d4f2843d
Yuhan Zhang <david8373@gmail.com>
2013-12-29 19:11:21 +0000

    fixed socket

--- web.js
+++ web.js
@@ -1,15 +1,27 @@
-var fs = require('fs');
-var express = require('express');
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    parser = new require('xml2json'),
+    fs = require('fs');
 
-var app = express.createServer(express.logger());
+app.listen(8000);
 
-var msg = fs.readFileSync('index.html').toString();
+function handler(req, res) {
+    fs.readFile(__dirname + '/client.html', function(err, data) {
+	if (err) {
+	    console.log(err);
+	    res.writeHead(500);
+	    return res.end('Error loading client.html');
+	}
+	res.writeHead(200);
+	res.end(data);
+    });
+}
 
-app.get('/', function(request, response) {
-  response.send(msg);
+io.sockets.on('connection', function(socket) {
+    setInterval(function() {
+	console.log('in a callback')
+	var json = JSON.parse('{"data":"hello"}')
+	socket.volatile.emit('notification', json);
+    }, 2000)
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
